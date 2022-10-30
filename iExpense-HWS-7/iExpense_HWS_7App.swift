150916d762e7d573f99726ec88ef33f25b743c0b
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-10-30 11:04:01 -0400

    Initial Commit

---
+++ iExpense-HWS-7/iExpense_HWS_7App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_HWS_7App.swift
+//  iExpense-HWS-7
+//
+//  Created by Kevin Mattocks on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_HWS_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
